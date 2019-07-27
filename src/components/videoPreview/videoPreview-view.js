1eb5496b2a6836a7c2794ea3e2c150cc76724a57
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-07-27 11:48:03 +0100

    mobile version

--- src/components/videoPreview/videoPreview-view.js
+++ src/components/videoPreview/videoPreview-view.js
@@ -7,12 +7,17 @@ export default class View extends React.PureComponent {
         // const { videoId, videoChannel, videoTitle, videoDate, videoCover } = this.props;
         const { videoId, videoTitle, videoCover } = this.props;
         return (
-            <div className="text-center mt-4">
-                <img className="Video-cover" src={videoCover} alt="YouLoader Logo" />
-                <div className="Video-title mb-2">{videoTitle}</div>
-                {/* <div className="Video-channel">By: {videoChannel}</div> */}
-                {/* <div className="Video-date">Published at: {new Date(videoDate).toLocaleDateString('en-US')}</div> */}
-                <DownloadButton id={videoId} />
+            <div>
+                <div className="d-none d-md-block text-center mt-4">
+                    <img className="Video-cover" src={videoCover} alt="YouLoader Logo" />
+                    <div className="Video-title mb-2">{videoTitle}</div>
+                    {/* <div className="Video-channel">By: {videoChannel}</div> */}
+                    {/* <div className="Video-date">Published at: {new Date(videoDate).toLocaleDateString('en-US')}</div> */}
+                    <DownloadButton id={videoId} />
+                </div>
+                <div className="d-block d-md-none text-center mt-4">
+                    <DownloadButton id={videoId} />
+                </div>
             </div>
         );
     }
