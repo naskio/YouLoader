2aba567b1e50d6c5b45e0918b03d704d1db25267
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-08-14 22:53:45 +0100

    Updates

---
+++ precache-manifest.e7edeca204ccb88b1da52b21d15e15eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ed7f07974d3073e5b4f228921cee61",
+    "url": "/YouLoader/index.html"
+  },
+  {
+    "revision": "3899ff1c54809558622c",
+    "url": "/YouLoader/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e2b8a4ab74457c2917c1",
+    "url": "/YouLoader/static/css/main.2128e35c.chunk.css"
+  },
+  {
+    "revision": "3899ff1c54809558622c",
+    "url": "/YouLoader/static/js/2.cce13f29.chunk.js"
+  },
+  {
+    "revision": "e2b8a4ab74457c2917c1",
+    "url": "/YouLoader/static/js/main.c3c1d7b6.chunk.js"
+  },
+  {
+    "revision": "050279b8df655aa7c180",
+    "url": "/YouLoader/static/js/runtime~main.68a657a2.js"
+  },
+  {
+    "revision": "fcfad3028f22600bbfaa0fa15faadafe",
+    "url": "/YouLoader/static/media/logo.fcfad302.svg"
+  },
+  {
+    "revision": "2b1c8be0bf2d80517a316aeae5e70468",
+    "url": "/YouLoader/static/media/outline-clear-24px.2b1c8be0.svg"
+  },
+  {
+    "revision": "59d1a698adab937dbe33fa7acecce228",
+    "url": "/YouLoader/static/media/outline-info-24px.59d1a698.svg"
+  },
+  {
+    "revision": "8206b64572558dc986dcc35a2333eb82",
+    "url": "/YouLoader/static/media/outline-search-24px.8206b645.svg"
+  }
+]);
